90692388f05aa521a83fcffc84a8efdd00b68376
7starrogan <oganifeose@gmail.com>
2022-10-22 14:12:02 -0700

    1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,15 +3,19 @@
 /**
 *_isupper - uppercase letter
 *
-*@c: char to check 
+*@c:char to check
 *
 *Return: 0 or 1
 */
 
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-return (1);
-else
-return (0);
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
