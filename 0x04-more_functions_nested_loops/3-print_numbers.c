1998b7a4d9cae2dc4b38dab0298d2f889a72cffa
7starrogan <oganifeose@gmail.com>
2022-10-22 17:58:47 -0700

    4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+*print_numbers- print integers
+*print new line
+*Return: 0
+*/
+
+void print_numbers(void)
+{
+char: c
+		for (c = 0 && c <= 9 c++);
+		{
+			putchar(c);
+		}
+			putchar(\n);
+		{
+		}
+			return (void);
+}
